348e665189a60ee7d7dc39647c1b3ec34b26e9a8
Vetsou <virees@posteo.net>
2025-06-19 00:10:26 +0200

    Added initial string implementation

---
+++ src/include/object.h
@@ -0,0 +1,36 @@
+#ifndef _OBJECT_H_
+#define _OBJECT_H_
+
+#include "types.h"
+#include "values.h"
+
+typedef enum {
+    OBJ_STRING,
+} obj_type_t;
+
+struct obj_t {
+    obj_type_t type;
+};
+
+#define OBJ_TYPE(value) (AS_OBJ(value)->type)
+
+//
+// STRING OBJECT
+//
+
+struct obj_str_t {
+    obj_t obj;
+    int32_t len;
+    char *chars;
+};
+
+static inline bool is_obj_type(value_t value, obj_type_t type) {
+    return IS_OBJ(value) && AS_OBJ(value)->type == type;
+}
+
+#define IS_STRING(value)  is_obj_type(value, OBJ_STRING)
+
+#define AS_STRING(value)  ((obj_str_t*)AS_OBJ(value))
+#define AS_CSTRING(value) (((obj_str_t*)AS_OBJ(value))->chars)
+
+#endif // _OBJECT_H_
--- src/include/values.h
+++ src/include/values.h
@@ -3,10 +3,16 @@
 
 #include "types.h"
 
+// Forward declaration object.h
+typedef struct obj_t obj_t;
+typedef struct obj_str_t obj_str_t;
+// Forward declaration END
+
 typedef enum {
     VAL_BOOL,
     VAL_NIL,
-    VAL_NUMBER
+    VAL_NUMBER,
+    VAL_OBJ
 } value_type_t;
 
 typedef struct {
@@ -14,6 +20,7 @@ typedef struct {
     union {
         bool boolean;
         double number;
+        obj_t *obj;
     } as;
 } value_t;
 
@@ -22,13 +29,16 @@ bool values_equal(value_t a, value_t b);
 #define IS_BOOL(value)    ((value).type == VAL_BOOL)
 #define IS_NIL(value)     ((value).type == VAL_NIL)
 #define IS_NUMBER(value)  ((value).type == VAL_NUMBER)
+#define IS_OBJ(value)     ((value).type == VAL_OBJ)
 
 #define AS_BOOL(value)    ((value).as.boolean)
 #define AS_NUMBER(value)  ((value).as.number)
+#define AS_OBJ(value)     ((value).as.obj)
 
 #define BOOL_VAL(value)   ((value_t){VAL_BOOL, {.boolean = value}})
 #define NIL_VAL           ((value_t){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((value_t){VAL_NUMBER, {.number = value}})
+#define OBJ_VAL(object)   ((value_t){VAL_OBJ, {.obj = (obj_t*)object}})
 
 typedef struct {
     size_t len;
